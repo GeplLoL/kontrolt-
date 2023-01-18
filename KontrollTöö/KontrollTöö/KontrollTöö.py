d905600812df796cd27efd89b910e20640e50a4b
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-01-18 12:25:26 +0200

    Add files via upload

---
+++ KontrollTöö/KontrollTöö/KontrollTöö.py
@@ -0,0 +1,21 @@
+﻿from random import *
+#n = input("linnumajade arv: ")
+#while n.isdigit()==False or int(n)>9 or int(n)<0:
+#    n = input("linnumajade arv: ")
+#for i in range(0,int(n),1):
+#    print(" -----\n!  0  !\n!  -  !\n -----")
+#    print("")
+#print(" ")
+
+#a = input("alus: ")
+#while a.isdigit()==False:
+#    a = input("alus: ")
+#b = input("eksponent: ")
+#while b.isdigit()==False:
+#    b = input("eksponent: ")
+#print(f"teie loomulik arv: {int(a)**int(b)}")
+#print(" ")
+
+#opilane = randint(1,30)
+#hinne = randint(1,5)
+#print(f"õpilaste keskmine hinne: {float(opilane)*float(hinne)/float(opilane)}")
