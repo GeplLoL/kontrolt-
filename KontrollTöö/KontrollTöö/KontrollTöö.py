a94aa33fc6715dd274d47851cf34b0c6f5933532
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-01-19 15:32:55 +0300

    Update KontrollTöö.py

--- KontrollTöö/KontrollTöö/KontrollTöö.py
+++ KontrollTöö/KontrollTöö/KontrollTöö.py
@@ -1,12 +1,16 @@
 ﻿from random import *
-#n = input("linnumajade arv: ")
-#while n.isdigit()==False or int(n)>9 or int(n)<0:
-#    n = input("linnumajade arv: ")
-#for i in range(0,int(n),1):
-#    print(" -----\n!  0  !\n!  -  !\n -----")
-#    print("")
-#print(" ")
-
+#s = input("linnumajade arv: ")
+#while s.isdigit()==False or int(s)>9 or int(s)<0:
+#    s = input("linnumajade arv: ")
+#for i in range(1,int(s)+1):
+#    print(' -----', end="     ")
+#print("\n",end="")
+#for i in range(1,int(s)+1):
+#    print("!  -  !", end="    ")
+#print("\n",end="")
+#for i in range(1,int(s)+1):
+#    print(" -----", end="     ")
+#print("\n")
 #a = input("alus: ")
 #while a.isdigit()==False:
 #    a = input("alus: ")
@@ -15,7 +19,21 @@
 #    b = input("eksponent: ")
 #print(f"teie loomulik arv: {int(a)**int(b)}")
 #print(" ")
-
 #opilane = randint(1,30)
 #hinne = randint(1,5)
 #print(f"õpilaste keskmine hinne: {float(opilane)*float(hinne)/float(opilane)}")
+#m=0
+#y=0
+#x = input("kui palju raha tahad saada? ")
+#while m<int(x):   
+#    y+=1
+#    m+=y
+#    print(m)
+#m=y=0
+#x = input("kui vanaks sa saad? ")
+#while x.isdigit()==False:
+#    x = input("kui vanaks sa saad? ")
+#for i in range(0,int(x),1):
+#    y+=1
+#    m+=y
+#print(f"nii palju sa teenid: {m}$")
