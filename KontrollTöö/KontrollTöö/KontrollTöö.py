dfe24d8eecb2d00c814fae0052f2663152bc4be9
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-01-23 12:43:22 +0200

    Update KontrollTöö.py

--- KontrollTöö/KontrollTöö/KontrollTöö.py
+++ KontrollTöö/KontrollTöö/KontrollTöö.py
@@ -1,4 +1,5 @@
 ﻿from random import *
+#1
 #s = input("linnumajade arv: ")
 #while s.isdigit()==False or int(s)>9 or int(s)<0:
 #    s = input("linnumajade arv: ")
@@ -11,6 +12,7 @@
 #for i in range(1,int(s)+1):
 #    print(" -----", end="     ")
 #print("\n")
+#2
 #a = input("alus: ")
 #while a.isdigit()==False:
 #    a = input("alus: ")
@@ -18,10 +20,12 @@
 #while b.isdigit()==False:
 #    b = input("eksponent: ")
 #print(f"teie loomulik arv: {int(a)**int(b)}")
+#3
 #print(" ")
 #opilane = randint(1,30)
 #hinne = randint(1,5)
 #print(f"õpilaste keskmine hinne: {float(opilane)*float(hinne)/float(opilane)}")
+#4
 #m=y=0
 #x = input("kui vanaks sa saad? ")
 #while x.isdigit()==False:
@@ -30,6 +34,7 @@
 #    y+=1
 #    m+=y
 #print(f"nii palju sa teenid: {m}$")
+#5
 #n =0
 #m =1
 #print("0","1 " ,end="")
